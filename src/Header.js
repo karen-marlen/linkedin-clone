56d739eca4a17d687cbfb1828cc4a5ef591db329
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-01-08 18:31:19 -0300

    header-right

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,12 @@ import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import ChatIcon from '@material-ui/icons/Chat';
 
 import HeaderOption from './HeaderOption';
+
 
 function Header() {
     return (
@@ -23,6 +27,9 @@ function Header() {
             <div className="header__right">
                 <HeaderOption Icon={HomeIcon} title='Home' />
                 <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+                <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+                <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+                <HeaderOption Icon={ChatIcon} title='Message' />
             </div>
         </div>
     )
