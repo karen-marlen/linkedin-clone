bb7a48371b1ab1d7cbc01b3382814f6744f41ffb
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-01-08 21:05:59 -0300

    css sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,7 +6,7 @@ function Sidebar() {
     return (
         <div className='sidebar'>
             <div className="sidebar__top">
-                <img src='' alt='' />
+                <img src="https://images.unsplash.com/photo-1586745370973-343d1ba81ed2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8bWluaW1hbGlzdCUyMGtleWJvYXJkfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='' />
                 <Avatar className='sidebar__avatar' />
                 <h2>Karen Marlen</h2>
                 <h4>karenzhec@gmail.com</h4>
@@ -15,11 +15,11 @@ function Sidebar() {
             <div className="sidebar__stats">
                 <div className="sidebar__stat">
                     <p>Who viewed you</p>
-                    <p className="sidebar__statNumer">2,33</p>
+                    <p className="sidebar__statNumber">2,33</p>
                 </div>
                 <div className="sidebar__stat">
                     <p> Views on post</p>
-                    <p className="sidebar__statNumer">2,77</p>
+                    <p className="sidebar__statNumber">2,77</p>
                 </div>
             </div>
 
