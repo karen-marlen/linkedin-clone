5c369187897e7cddeeefcd35abab08b6f70e2f70
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-01-10 01:33:25 -0300

    feed : post

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Feed.css';
+
 import CreateIcon from '@material-ui/icons/Create';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 
 function Feed() {
     return (
@@ -11,13 +16,38 @@ function Feed() {
 
                     <form>
                         <input type="text"/>
-                        <button>Send</button>
+                        <button type='submit'>Send</button>
                     </form>
                 </div>
+
+                <div className="feed__inputOptions">
+                    <InputOption 
+                        Icon={ImageIcon} 
+                        title='Photo'
+                        color='#70B5F9'                 
+                    />
+                    <InputOption 
+                        Icon={SubscriptionsIcon} 
+                        title='Video'
+                        color='#EFA33E'                 
+                    />
+                    <InputOption 
+                        Icon={EventNoteIcon} 
+                        title='Event'
+                        color='#C0CBCD'                 
+                    />
+                    <InputOption 
+                        Icon={CalendarViewDayIcon} 
+                        title='Write article'
+                        color='#7FC15E'                 
+                    />
+                </div>
             </div>
+
+            
             
         </div>
-    )
+    );
 }
 
 export default Feed;
