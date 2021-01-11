e0ff73a0250dc978680b590cb2e42af600327796
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-01-11 13:19:00 -0300

    login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import './App.css';
 
+import { selectUser } from './features/counter/userSlice';
+
+import Login from './Login';
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 function App() {
+
+  const user = useSelector(selectUser)
+
   return (
     <div className='app'>
       <Header />
 
+      {!user ? (
+        <Login />
+      ): (
+        <div className='app__body'>
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
 
-      <div className='app__body'>
-        <Sidebar />
-        <Feed />
-      </div>
     </div>
   );
 }
